fdde30e8a0560c47d8c911706907f420e13b2100
hcmute-toan <116945403+hcmute-toan@users.noreply.github.com>
2025-03-18 22:07:55 +0700

    project initiation

---
+++ src/test/java/com/fourt/RailSkylines/RailSkylinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourt.RailSkylines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailSkylinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
