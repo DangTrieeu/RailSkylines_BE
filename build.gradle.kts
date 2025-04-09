811322722d676cf42b35444eb5901ae10ea170be
DangTrieeu <dangtrieu.lb@gmail.com>
2025-04-09 11:13:30 +0700

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,20 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // Updated JPA dependency with Jakarta
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("mysql:mysql-connector-java:8.0.33") // Add this line
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // Updated MySQL connector for runtime
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
