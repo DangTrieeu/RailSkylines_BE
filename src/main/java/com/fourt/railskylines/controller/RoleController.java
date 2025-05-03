868054af94115aa54e7922ceda434e544012ad26
hcmute-toan <116945403+hcmute-toan@users.noreply.github.com>
2025-05-03 11:55:23 +0700

    fix

---
+++ src/main/java/com/fourt/railskylines/controller/RoleController.java
@@ -0,0 +1,94 @@
+package com.fourt.railskylines.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fourt.railskylines.domain.Role;
+import com.fourt.railskylines.domain.response.ResultPaginationDTO;
+import com.fourt.railskylines.service.RoleService;
+import com.fourt.railskylines.util.annotation.APIMessage;
+import com.fourt.railskylines.util.error.IdInvalidException;
+import com.turkraft.springfilter.boot.Filter;
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostMapping("/roles")
+    @APIMessage("Create a role")
+    public ResponseEntity<Role> create(@Valid @RequestBody Role r) throws IdInvalidException {
+        // check name
+        if (this.roleService.existByName(r.getName())) {
+            throw new IdInvalidException("Role với name = " + r.getName() + " đã tồn tại");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.roleService.create(r));
+    }
+
+    @PutMapping("/roles")
+    @APIMessage("Update a role")
+    public ResponseEntity<Role> update(@Valid @RequestBody Role r) throws IdInvalidException {
+        // check id
+        if (this.roleService.fetchById(r.getId()) == null) {
+            throw new IdInvalidException("Role với id = " + r.getId() + " không tồn tại");
+        }
+
+        // check name
+        // if (this.roleService.existByName(r.getName())) {
+        // throw new IdInvalidException("Role với name = " + r.getName() + " đã tồn
+        // tại");
+        // }
+
+        return ResponseEntity.ok().body(this.roleService.update(r));
+    }
+
+    @DeleteMapping("/roles/{id}")
+    @APIMessage("Delete a role")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id) throws IdInvalidException {
+        // check id
+        if (this.roleService.fetchById(id) == null) {
+            throw new IdInvalidException("Role với id = " + id + " không tồn tại");
+        }
+        this.roleService.delete(id);
+        return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping("/roles")
+    @APIMessage("Fetch roles")
+    public ResponseEntity<ResultPaginationDTO> getPermissions(
+            @Filter Specification<Role> spec, Pageable pageable) {
+
+        return ResponseEntity.ok(this.roleService.getRoles(spec, pageable));
+    }
+
+    @GetMapping("/roles/{id}")
+    @APIMessage("Fetch role by id")
+    public ResponseEntity<Role> getById(@PathVariable("id") long id) throws IdInvalidException {
+
+        Role role = this.roleService.fetchById(id);
+        if (role == null) {
+            throw new IdInvalidException("Resume với id = " + id + " không tồn tại");
+        }
+
+        return ResponseEntity.ok().body(role);
+    }
+
+}
+
---
+++ src/main/java/com/fourt/railskylines/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.fourt.railskylines.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.fourt.railskylines.domain.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>,
+                JpaSpecificationExecutor<Role> {
+        boolean existsByName(String name);
+
+        Role findByName(String name);
+}
